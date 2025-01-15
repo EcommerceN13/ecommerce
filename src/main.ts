8d76869f881957bbfead52af770484a9c1715916
Sardorbek1712 <sardor.tolibov04@gmail.com>
2025-01-15 15:47:40 +0500

    Develop branch yaratildi!

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,7 @@ import { BadRequestException, ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const configService = app.get(ConfigService)
-
+  const configService = app.get(ConfigService);
 
   app.useGlobalPipes(
     new ValidationPipe({
@@ -23,20 +22,21 @@ async function bootstrap() {
 
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
 
-  app.use(morgan('tiny'))
+  app.use(morgan('tiny'));
 
   const config = new DocumentBuilder()
-    .setTitle('Feane restaurant API')
-    .setDescription('The feane API description')
+    .setTitle('Ashyo API')
+    .setDescription('Ashyo API description')
     .setVersion('1.0')
     .addBearerAuth()
     .build();
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
-
   await app.listen(configService.get<number>('appConfig.port'), () => {
-    console.log(`Uraaa server ${configService.get<number>('appConfig.port')} portda ishlamoqda...`);
+    console.log(
+      `Uraaa server ${configService.get<number>('appConfig.port')} portda ishlamoqda...`,
+    );
   });
 }
 bootstrap();
