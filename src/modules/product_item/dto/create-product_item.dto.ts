15c00932fdef901abbe370cdb2d380bec1a1bb07
bakxriyev <kamronbekbahriyev18@gmail.com>
2025-01-16 16:36:27 +0500

    crudlar boldi

--- src/modules/product_item/dto/create-product_item.dto.ts
+++ src/modules/product_item/dto/create-product_item.dto.ts
@@ -1,1 +1,17 @@
-export class CreateProductItemDto {}
+import { IsNotEmpty, IsNumber, IsPositive, IsString } from 'class-validator';
+
+export class CreateProductItemDto {
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  price: number;
+
+  @IsNotEmpty()
+  @IsString()
+  image: string;
+
+  @IsNotEmpty()
+  @IsNumber()
+  @IsPositive()
+  product_id: number;
+}
--- src/modules/product_item/product_item.controller.ts
+++ src/modules/product_item/product_item.controller.ts
@@ -1,9 +1,9 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { ProductItemService } from './product_item.service';
-import { CreateProductItemDto } from './dto/create-product_item.dto';
-import { UpdateProductItemDto } from './dto/update-product_item.dto';
+import { CreateProductItemDto } from './dto';
+import { UpdateProductItemDto } from './dto';
 
-@Controller('product-item')
+@Controller('product-items')
 export class ProductItemController {
   constructor(private readonly productItemService: ProductItemService) {}
 
@@ -18,17 +18,17 @@ export class ProductItemController {
   }
 
   @Get(':id')
-  findOne(@Param('id') id: string) {
+  findOne(@Param('id') id: number) {
     return this.productItemService.findOne(+id);
   }
 
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductItemDto: UpdateProductItemDto) {
+  update(@Param('id') id: number, @Body() updateProductItemDto: UpdateProductItemDto) {
     return this.productItemService.update(+id, updateProductItemDto);
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.productItemService.remove(+id);
+  delete(@Param('id') id: number) {
+    return this.productItemService.delete(+id);
   }
 }
