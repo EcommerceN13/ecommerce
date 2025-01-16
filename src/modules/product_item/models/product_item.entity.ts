88a31cbd804b64c5505a9aa24ee7c8ece6ce2d5d
Zeboavazbekovna <avazbekovna.zebo@email.com>
2025-01-16 15:20:36 +0500

    product configuration va variation_optionning ulanishlari qilindi

--- src/modules/product_item/models/product_item.entity.ts
+++ src/modules/product_item/models/product_item.entity.ts
@@ -1,13 +1,18 @@
-import { Table, Model, Column, DataType } from "sequelize-typescript";
+import { Table, Model, Column, DataType, HasMany } from "sequelize-typescript";
+import { ProductConfiguration } from "src/modules/product_configuration";
 
-@Table({ tableName: 'variation', timestamps: true })
-export class Variation extends Model {
+
+@Table({ tableName: 'product_item', timestamps: true })
+export class ProductItem extends Model {
     @Column({ type: DataType.BIGINT, allowNull: false })
-    category_id: number;
+    price: number;
 
     @Column({ type: DataType.STRING, allowNull: false })
-    name: string;
+    image: string;
 
-    @Column({ type: DataType.STRING, allowNull: false })
-    color: string;
+    @Column({ type: DataType.BIGINT, allowNull: false })
+    product_id: number;
+
+    @HasMany(() => ProductConfiguration)
+    configurations: ProductConfiguration[];
 }
--- src/modules/variation_option/models/variation_option.model.ts
+++ src/modules/variation_option/models/variation_option.model.ts
@@ -1,10 +1,15 @@
 import { Table, Model, Column, DataType, HasMany } from "sequelize-typescript";
+import { ProductConfiguration } from "src/modules/product_configuration";
+
 
 @Table({ tableName: 'variation_option', timestamps: true })
 export class VariationOption extends Model {
     @Column({ type: DataType.BIGINT, allowNull: false })
     variation_id: number;
 
-    @Column({ type: DataType.STRING, allowNull: false})
+    @Column({ type: DataType.STRING, allowNull: false })
     value: string;
+
+    @HasMany(() => ProductConfiguration)
+    configurations: ProductConfiguration[];
 }
