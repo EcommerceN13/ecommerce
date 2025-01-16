7332c08e8b240e36eb967d7f1b4ed7bfe9f48d7d
Abduqodir65 <abduqodirgithub@gmail.com>
2025-01-16 14:58:18 +0500

    Implement Like module with DTOs, interfaces, controller, and service

--- src/modules/like/models/like.model.ts
+++ src/modules/like/models/like.model.ts
@@ -1,10 +1,13 @@
-import { Column, DataType, Model, Table } from "sequelize-typescript";
+import { Column, DataType, ForeignKey, Model, Table } from "sequelize-typescript";
+import { User } from "src/modules/user";
 
 @Table({tableName:"like",timestamps:true})
 export class Like extends Model {
-    @Column({type: DataType.BIGINT, allowNull: false,})
+
+    @ForeignKey(() => User)
+    @Column({type: DataType.INTEGER, allowNull: false,onDelete: "CASCADE", onUpdate: "CASCADE"})
     user_id: number;
 
-    @Column({type: DataType.BIGINT, allowNull: false,})
+    @Column({type: DataType.INTEGER, allowNull: false,onDelete: "CASCADE", onUpdate: "CASCADE"})
     product_id: number
 }
